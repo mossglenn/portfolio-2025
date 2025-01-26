5ac8a0d792f4892a22ed82953ba1ad9a8a1e56e2
Amos Glenn <amos@amosglenn.com>
2025-01-25 21:30:05 -0500

    formatting for build error

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
@@ -21,6 +21,6 @@ const nextConfig: NextConfig = {
       ],
     },
   ],
-};
+}
 
-export default nextConfig;
+export default nextConfig
